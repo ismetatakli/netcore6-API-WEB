3924d965021697a71c19a24ec8003bdeecf54d76
ismetatakli <ismetatakli@gmail.com>
2022-08-28 22:31:00 +0300

    first initial

---
+++ Core6App.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Core6App.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

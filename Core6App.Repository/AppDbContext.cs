0240cee06ea732832952c7cdb117c95e766a1f58
ismetatakli <ismetatakli@gmail.com>
2022-08-30 02:10:47 +0300

    AppDbContext created

---
+++ Core6App.Repository/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Core6App.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core6App.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}

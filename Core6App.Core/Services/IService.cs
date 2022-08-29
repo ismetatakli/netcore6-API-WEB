7acd49d3993450fd8e76ed7a4a22894d257fa553
ismetatakli <ismetatakli@gmail.com>
2022-08-30 01:57:37 +0300

    IService created

---
+++ Core6App.Core/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Core6App.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}

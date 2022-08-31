a6027e7c17a02e7c41deac8142c473f2838bb735
ismetatakli <ismetatakli@gmail.com>
2022-09-01 00:04:08 +0300

    AutoMapper added and MapProfile created

--- Core6App.API/Program.cs
+++ Core6App.API/Program.cs
@@ -1,8 +1,11 @@
 using Core6App.Core.Repositories;
+using Core6App.Core.Services;
 using Core6App.Core.UnitOfWorks;
 using Core6App.Repository;
 using Core6App.Repository.Repositories;
 using Core6App.Repository.UnitOfWorks;
+using Core6App.Service.Mappings;
+using Core6App.Service.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -17,6 +20,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
---
+++ Core6App.Service/Mappings/MapProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Core6App.Core.DTOs;
+using Core6App.Core.Models;
+
+namespace Core6App.Service.Mappings
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}

a54834c67ec7a07fa5bc8a9a950e73d90e6d7e51
ismetatakli <ismetatakli@gmail.com>
2022-09-01 00:21:26 +0300

    Response DTOs created(w data and without data)

---
+++ Core6App.Core/DTOs/NoContentDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core6App.Core.DTOs
+{
+    public class NoContentDto
+    {
+        public List<String> Errors { get; set; }
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public static NoContentDto Success(int statusCode)
+        {
+            return new NoContentDto { StatusCode = statusCode};
+        }
+        public static NoContentDto Fail(int statusCode, List<string> errors)
+        {
+            return new NoContentDto { StatusCode = statusCode, Errors = errors };
+        }
+        public static NoContentDto Fail(int statusCode, string error)
+        {
+            return new NoContentDto { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+    }
+}

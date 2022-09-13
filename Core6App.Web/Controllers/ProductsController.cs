fde706fa1875e4df8987ea008dcdc1147e36e91f
ismetatakli <ismetatakli@gmail.com>
2022-09-13 21:41:24 +0300

    ProductController Save method created

--- Core6App.Web/Controllers/ProductsController.cs
+++ Core6App.Web/Controllers/ProductsController.cs
@@ -1,20 +1,51 @@
-﻿using Core6App.Core.Services;
+﻿using AutoMapper;
+using Core6App.Core.DTOs;
+using Core6App.Core.Models;
+using Core6App.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Core6App.Web.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _productService.GetProductsWithCategory());
         }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+
     }
 }
